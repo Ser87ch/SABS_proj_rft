1470a0ef89763b6bb4519851fc3907ec8d7e71a8
SerVM <ser4@me.com>
2013-07-01 16:43:47 +0400

    TM

--- SABS/RunTestCase.java
+++ SABS/RunTestCase.java
@@ -3,6 +3,7 @@ import resources.SABS.RunTestCaseHelper;
 import ru.sabstest.ClientList;
 import ru.sabstest.DeltaDB;
 import ru.sabstest.Init;
+import ru.sabstest.Log;
 import ru.sabstest.ModuleList;
 import ru.sabstest.Settings;
 import ru.sabstest.TestCase;
@@ -18,6 +19,8 @@ import com.rational.test.ft.object.interfaces.generichtmlsubdomain.*;
 import com.rational.test.ft.script.*;
 import com.rational.test.ft.value.*;
 import com.rational.test.ft.vp.*;
+import com.rational.test.tss.TSSException;
+import com.rational.test.tss.TSSUtility;
 import com.ibm.rational.test.ft.object.interfaces.sapwebportal.*;
 
 public class RunTestCase extends RunTestCaseHelper
@@ -25,8 +28,23 @@ public class RunTestCase extends RunTestCaseHelper
 
 	public void testMain(Object[] args) 
 	{
-		Settings.testProj = (String) args[0];
-		String src = "C:\\sabstest\\settings\\test\\" + (String) args[1] + ".xml";
+		String num="";
+		try {
+			Settings.testProj = TSSUtility.getScriptOption("projFolder");
+			num = TSSUtility.getScriptOption("num");
+		} catch (TSSException e) {
+			e.printStackTrace();
+			Log.msg(e);			
+		}		
+
+		if(Settings.testProj == null)
+			Settings.testProj = (String) args[0];
+
+
+		if(num == null)
+			num = (String) args[1];
+
+		String src = Settings.testProj + "settings\\test\\" + num + ".xml";
 
 		Init.load();
 		Settings.readXML(Settings.testProj + "settings\\general.xml");
@@ -41,7 +59,7 @@ public class RunTestCase extends RunTestCaseHelper
 
 		for(int i = 0; i < t.getSize(); i++)
 		{
-			callScript(t.getScript(i), new Object[]{t.getStep(i).options, (String) args[1]});
+			callScript(t.getScript(i), new Object[]{t.getStep(i).options, num});
 		}
 
 	}
